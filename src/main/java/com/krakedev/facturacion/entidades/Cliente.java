86ce0e9c36ff629ae18b8f6eb9db3a39370a1028
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-05-09 12:50:59 -0400

    feat(cliente): add attributes, constructors, getters, setters and toString method

---
+++ src/main/java/com/krakedev/facturacion/entidades/Cliente.java
@@ -0,0 +1,64 @@
+package com.krakedev.facturacion.entidades;
+
+public class Cliente {
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String telefono;
+
+	public Cliente(String cedula, String nombre, String apellido, String email, String telefono) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
+				+ ", telefono=" + telefono + "]";
+	}
+
+}
